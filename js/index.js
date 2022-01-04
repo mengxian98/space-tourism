cfb4bc524b8e06beb11784ff0a941fcb8de10d29
Wong Meng Xian <mengxian98@gmail.com>
2022-01-04 18:45:40 +0800

    Add Destination and Crew Assets & Destination Tablist Behaviour

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Primary Navigation
 const navMenu = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
@@ -21,3 +22,76 @@ navToggle.addEventListener("click", function() {
     navToggle.style.animation = "";
   }
 });
+
+
+// Tab List
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = document.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener("keydown", updateTabFocus);
+
+tabs.forEach((tab) => {
+  // Mouse Click
+  tab.addEventListener("click", function(event) {
+    updateActivePanel(event.target);
+  });
+  // Keyboard Control - Enter Key
+  tab.addEventListener("keydown", function(event) {
+    const keydownEnter = 13;
+    if (event.keyCode === keydownEnter) {
+      updateActivePanel(event.target);
+    }
+  });
+})
+
+let tabFocus = 0;
+
+function updateTabFocus(event) {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+  // Move Left and Right
+  if (event.keyCode === keydownLeft || event.keyCode === keydownRight) {
+    // Unfocus Original Tab
+    tabs[tabFocus].setAttribute("tabindex", "-1");
+    // Check Left or Right Key
+    if (event.keyCode === keydownLeft) {
+      if (tabFocus === 0) {
+        tabFocus = tabs.length - 1;
+      } else {
+        tabFocus--;
+      }
+    } else {
+      if (tabFocus === tabs.length - 1) {
+        tabFocus = 0;
+      } else {
+        tabFocus++;
+      }
+    }
+
+    // Focus Selected Tab
+    tabs[tabFocus].setAttribute("tabindex", "0");
+    tabs[tabFocus].focus();
+  }
+}
+
+function updateActivePanel(targetTab) {
+  // Identify Target Panel
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const mainContainer = targetTab.parentNode.parentNode;
+
+  // Update Selection Tabs
+  targetTab
+    .parentNode.querySelectorAll('[role="tab"]')
+    .forEach((tab) => tab.setAttribute("aria-selected", "false"));
+
+  targetTab.setAttribute("aria-selected", "true");
+
+  // Remove Content
+  mainContainer
+    .querySelectorAll('[role="tabpanel"]')
+    .forEach((panel) => panel.setAttribute("hidden", true));
+
+  // Add Content
+  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+}
